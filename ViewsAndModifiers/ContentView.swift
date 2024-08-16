2380f3050441dc94a8df38265c89343d31fe7566
Tony Chang <tony@eliminated.org>
2024-08-15 19:36:22 -0700

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Tony Chang on 8/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Tony Chang on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
